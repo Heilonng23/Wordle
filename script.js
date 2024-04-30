62fc4652ddd9666733707da76db75b08c9789688
Ibragim <97885427+Heilonng23@users.noreply.github.com>
2024-04-30 11:54:07 +0200

    correct/wrong guesses

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const letters = document.querySelectorAll(".scoreboard-letter")
-const loading = document.querySelector(".info-bar")
+const loadingDiv = document.querySelector(".info-bar")
 const ANSWER_LENGTH = 5; 
 
-async function init(){
+async function init(){ 
    let currentGuess = '';
+   let currentRow = 0;
+
+
+   const res = await fetch("https://words.dev-apis.com/word-of-the-day")
+   const resObj = await res.json()
+   const word = resObj.word.toUpperCase()
+   const wordParts = word.split("")
+   setLoading(false)
+
+   console.log(word)
 
    function addLetter(letter){
       if(currentGuess.length < ANSWER_LENGTH){
@@ -12,12 +22,47 @@ async function init(){
          currentGuess = currentGuess.substring(0 , currentGuess.length - 1) + letter
       }
 
-      letters[currentGuess.length - 1].innerText = letter;
+      letters[ANSWER_LENGTH * currentRow +  currentGuess.length - 1].innerText = letter;
+   }
+   
+   async function commit(){
+      if(currentGuess.length !== ANSWER_LENGTH){
+         return;
+      }
+
+      const guessParts = currentGuess.split("")
+      const map = makeMap(wordParts)
+      console.log(map)
+
+      for(let i = 0 ; i < ANSWER_LENGTH; i++){
+         // mark as correct
+         if(guessParts[i] === wordParts[i]){
+            letters[currentRow * ANSWER_LENGTH + i].classList.add("correct")
+            map[guessParts[i]]--
+         }
+      }
+      for(let i = 0 ; i < ANSWER_LENGTH; i++){
+         if(guessParts[i] = wordParts[i]){
+            // do nothing because i already did it
+         } else if(wordParts.includes(guessParts[i])){
+            letters[currentRow * ANSWER_LENGTH + i].classList.add("close")
+         } else {
+            letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong")
+         }
+      }
+      currentRow++
+      currentGuess = '';
+   }
+   
+   function backspace(){
+      currentGuess = currentGuess.substring(0 , currentGuess.length - 1)
+      letters[ANSWER_LENGTH * currentRow +  currentGuess.length].innerText = "";
+
    }
 
    document.addEventListener("keydown", function handleKeyPress(event){
       const action = event.key;
-      console.log(action)
+      // console.log(action)
 
       if (action === 'Enter'){
          commit()
@@ -33,5 +78,23 @@ async function init(){
    function isLetter(letter) {
       return /^[a-zA-Z]$/.test(letter);
     }
+
+   function setLoading(isLoading){
+    loadingDiv.classList.toggle("show", isLoading)
+   }   
 }
+function makeMap(array){
+   const obj = {};
+
+   for(let i = 0 ; i < array.length; i++){
+      const letter = array[i]
+      if(obj[letter]){
+         obj[letter]++
+      }else{
+         obj[letter] = 1
+      }
+   }
+   return obj ; 
+}
+
 init()
